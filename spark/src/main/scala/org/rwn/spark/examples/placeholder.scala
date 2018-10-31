25776e0db3012178b5b10eeedd20fea85a9d6283
Radek Strejc <radek.strejc@oracle.com>
2018-10-31 13:39:26 +0100

    placehodler deleted

--- spark/src/main/scala/org/rwn/spark/examples/placeholder.scala
+++
@@ -1,8 +0,0 @@
-package org.rwn.spark.examples
-
-/**
-  * Created by Radek Strejc, radek.strejc at oracle.com on 10/28/18.
-  */
-class placeholder {
-
-}
--- spark/src/main/scala/org/rwn/spark/exercise/placeholder.scala
+++
@@ -1,6 +0,0 @@
-package org.rwn.spark.exercise
-
-/**
-  * Created by Radek Strejc, radek.strejc at oracle.com on 10/28/18.
-  */
-class placeholder {}
